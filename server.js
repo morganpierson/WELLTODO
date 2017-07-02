ab99284bfe22dd8dbcb3a6415a4a47bba8a5e34f
Morgan Pierson <morganpierson@gmail.com>
2017-07-01 19:23:25 -0700

    webpack configured

---
+++ server.js
@@ -0,0 +1,14 @@
+"use strict";
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  watch: true, //webpack recompile bundle if any changes made
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      }
+    ]
+  }
+}
